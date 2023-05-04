40c71d064e0f863b18baf0053c26f61a87c9bb5c
gregop <gregopdev@gmail.com>
2023-05-04 22:57:36 +0300

    CustomValidations Creating Custom validator (StrictlyPositive) for WorkoutMode,

---
+++ Models/Validators/WorkoutModelValidators.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNSL_WepService.Models.Validators.WorkoutModelValidators
+{
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class StrictlyPositive : ValidationAttribute
+    {
+        private readonly string _property_name;
+        public StrictlyPositive(string property_name)
+        {
+            _property_name = property_name;
+        }
+
+        protected override ValidationResult? IsValid(object ?value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult($"The {_property_name} field is required.");
+            }
+
+            // check if value is number
+            bool isNumber = int.TryParse(_property_name, out int number);
+            
+            if (isNumber && number > 0)
+            {
+                return ValidationResult.Success;
+                
+            }
+            else
+            {
+                return new ValidationResult($"{_property_name} field must be positive integer.");
+                
+            }
+
+            
+        }
+    }
+}
