e4df83393b9c92e18a60dd715451dce448a82485
gregop <gregopdev@gmail.com>
2023-05-28 17:13:30 +0300

    Validation Helper Class
    ValidationHelper class to be used for model Validation.
    - instantiates a ValidationHelper object with an empty list of strings
    - has method to add errors, get errors, and boolean  method isValid to check if model is valid

---
+++ Models/Validators/ValidationHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNSL_WepService.Models.Validators
+{
+    public class ValidationHelper
+    {
+        private List<string> _validationErrors;
+
+        public ValidationHelper()
+        {
+            _validationErrors = new List<string>();
+        }
+
+        public bool IsValid()
+        {
+            return _validationErrors.Count() == 0;
+        }
+
+        public void AddError(string errorMessage)
+        {
+            _validationErrors.Add(errorMessage);
+        }
+
+        public List<string> GetErrorMessages()
+        {
+            return _validationErrors;
+        }
+
+    }
+}
