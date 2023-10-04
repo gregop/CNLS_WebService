23eaa1ddafdfedf059b40e386b5b631bef0e8447
gregop <gregopdev@gmail.com>
2023-10-05 00:14:28 +0300

    move DataObjects to root dir

---
+++ DataObjects/FitnessApp.Core.DataObjects/RegisterWorkoutDataObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessApp.Core.Validators;
+
+namespace FitnessApp.Core.DataObjects
+{
+    public class RegisterWorkoutDataObject
+    {
+        [StrictlyPositiveProperty("Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [StrictlyPositiveProperty("duration")]
+        public int Duration { get; set; }
+
+        [Required]
+        [StrictlyPositiveProperty("distance")]
+        public double Distance { get; set; }
+
+        [StrictlyPositiveProperty("calories")]
+        public int Calories { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool Cardio { get; set; }
+
+        public string? Description { get; set; }
+
+    }
+
+
+    public class GetWorkoutById
+    {
+        [Required]
+        [StrictlyPositiveProperty("Id")]
+        public int Id { get; set; }
+    }
+}
---
+++ DataObjects/FitnessApp.Core.DataObjects/WorkoutItemDataObject.cs
@@ -0,0 +1,34 @@
+﻿using FitnessApp.Core.Validators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Core.DataObjects
+{
+    public class WorkoutItemDataObject
+    {
+        [StrictlyPositiveProperty("Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [StrictlyPositiveProperty("duration")]
+        public int Duration { get; set; }
+
+        [Required]
+        [StrictlyPositiveProperty("distance")]
+        public double Distance { get; set; }
+
+        [StrictlyPositiveProperty("calories")]
+        public int Calories { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public bool Cardio { get; set; }
+
+        public string? Description { get; set; }
+
+    }
+}
