0e0cb41778a518fcd7548cae7a7d4d0bddc2f2a2
gregop <gregopdev@gmail.com>
2023-10-05 23:50:43 +0300

    Introducing Generics to OperasionalResult class

--- Validators/FitnessApp.Core.Validators/OperationalResult.cs
+++ Validators/FitnessApp.Core.Validators/OperationalResult.cs
@@ -1,22 +1,17 @@
-﻿namespace FitnessApp.Core.Validators
+﻿using System.ComponentModel;
+
+namespace FitnessApp.Core.Validators
 {
-    public class OperationalResult
+    public class OperationalResult<T>
     {
         public bool IsSuccessfulOperation { get; protected set; }
+        public T? Data { get; protected set; }
         public string? FailureMessage { get; protected set; }
         public Exception? Exception { get; protected set; }
-        public object? Data { get; protected set; }
 
-        public OperationalResult() 
+        public OperationalResult(bool isSuccessfulOperation = true, T? data = default)
         {
-            IsSuccessfulOperation = true;
-            FailureMessage = null;
-            Exception = null;
-            Data = null;
-        }
-        public OperationalResult(object data)
-        {
-            IsSuccessfulOperation = true;
+            IsSuccessfulOperation = isSuccessfulOperation;
             FailureMessage = null;
             Exception = null;
             Data = data;
@@ -27,7 +22,6 @@
             IsSuccessfulOperation = false;
             FailureMessage = messsage;
             Exception = null;
-            Data = null;
         }
 
         public OperationalResult(Exception ex)
@@ -35,23 +29,22 @@
             IsSuccessfulOperation = false;
             FailureMessage = ex.Message;
             Exception = ex;
-            Data = null;
         }
 
 
-        public static OperationalResult SuccessResult(object data)
+        public static OperationalResult<T> SuccessResult(T data)
         {
-            return new OperationalResult(data);
+            return new OperationalResult<T>(true, data);
         }
 
-        public static OperationalResult FailureResult(string message)
+        public static OperationalResult<T> FailureResult(string message)
         {
-            return new OperationalResult(message);
+            return new OperationalResult<T>(message);
         }
 
-        public static OperationalResult FailureResult(Exception ex)
+        public static OperationalResult<T> FailureResult(Exception ex)
         {
-            return new OperationalResult(ex);
+            return new OperationalResult<T>(ex);
         }
     }
 }
