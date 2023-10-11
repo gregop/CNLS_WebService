ae0ef6842f904269cb6113a00e8ceef126b0fc66
gregop <gregopdev@gmail.com>
2023-10-11 21:21:44 +0300

    Users Data Objects

---
+++ DataObjects/FitnessApp.Core.DataObjects/Users/UserDataObject.cs
@@ -0,0 +1,28 @@
+﻿using FitnessApp.Core.Validators;
+
+
+namespace FitnessApp.Core.DataObjects
+{
+    public class UserDataObject
+    {
+        [StrictlyPositiveProperty("Id")]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        [StrictlyPositiveProperty("Age")]
+        public int Age { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsLockedOut { get; set; }
+    }
+}
---
+++ DataObjects/FitnessApp.Core.DataObjects/Users/UserLoginActivityDataObject.cs
@@ -0,0 +1,20 @@
+﻿using FitnessApp.Core.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Core.DataObjects.Users
+{
+    public class UserLoginActivityDataObject
+    {
+        [StrictlyPositiveProperty("Id")]
+        public int Id { get; set; }
+
+        [StrictlyPositiveProperty("Id")]
+        public int UserId { get; set; }
+
+        public DateTime LoginDateTime { get; set; }
+    }
+}
