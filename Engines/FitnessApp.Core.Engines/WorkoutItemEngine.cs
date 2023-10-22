cd9113b554ccf1d9f7174e22416e803de97e9a28
gregop <gregopdev@gmail.com>
2023-10-22 10:37:35 +0300

    resource access project dependency for engines proj

---
+++ Engines/FitnessApp.Core.Engines/WorkoutItemEngine.cs
@@ -0,0 +1,29 @@
+﻿using FitnessApp.Core.DataObjects;
+using FitnessApp.Core.ResourceAccess;
+using FitnessApp.Core.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Core.Engines
+{
+    public class WorkoutItemEngine
+    {
+        private readonly WorkoutItemResourceAccess _workoutItemResourceAccess;
+
+        public WorkoutItemEngine(WorkoutItemResourceAccess workoutItemResourceAccess) 
+        {
+            _workoutItemResourceAccess = workoutItemResourceAccess;
+
+        }
+
+        //public async Task<OperationalResult<WorkoutItemDataObject>> HandleWorkoutCreationAsync(WorkoutItemDataObject workoutItemDataObject)
+        //{
+
+
+        //}
+
+    }
+}
