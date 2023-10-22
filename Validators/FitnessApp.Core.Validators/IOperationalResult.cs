14e6087a9be04c03bfdb8f1b4995193ddecfb361
gregop <gregopdev@gmail.com>
2023-10-22 12:41:35 +0300

    Create Interface for Operational Result

---
+++ Validators/FitnessApp.Core.Validators/IOperationalResult.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessApp.Core.Validators
+{
+    public interface IOperationalResult<T>
+    {
+        T? Data { get; }
+        Exception? Exception { get; }
+        string? FailureMessage { get; }
+        bool IsSuccessfulOperation { get; }
+
+    }
+}
