2b8b1c6bd856b47ed7e241c8102e94347137d4d8
gregop <gregopdev@gmail.com>
2023-10-22 12:43:02 +0300

    Create Interface and class for Response Context

---
+++ Validators/FitnessApp.Core.Validators/Interfaces/IResponseContext.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessApp.Core.Validators.Interfaces
+{
+    public interface IResponseContext<T>
+    {
+        T? Response { get; set; }
+        int StatusCode { get; set; }
+        string? StatusMessage { get; set; }
+    }
+}
---
+++ Validators/FitnessApp.Core.Validators/ResponseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessApp.Core.Validators.Interfaces;
+
+namespace FitnessApp.Core.Validators
+{
+    public class ResponseContext<T> : IResponseContext<T>
+    {
+        public int StatusCode { get; set; }
+        public string? StatusMessage { get; set; }
+        public T? Response { get; set; }
+
+    }
+}
