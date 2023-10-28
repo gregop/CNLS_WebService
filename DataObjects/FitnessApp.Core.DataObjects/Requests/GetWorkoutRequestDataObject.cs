de0566eb0ca26fdedf1e586b7e1299521c603ecc
gregop <gregopdev@gmail.com>
2023-10-28 13:08:30 +0300

    Data object for incoming get workout request data

---
+++ DataObjects/FitnessApp.Core.DataObjects/Requests/GetWorkoutRequestDataObject.cs
@@ -0,0 +1,17 @@
+﻿using FitnessApp.Core.Validators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Core.DataObjects.Requests
+{
+    public class GetWorkoutRequestDataObject
+    {
+        [Required]
+        [StrictlyPositiveProperty("Id")]
+        public int Id { get; set; }
+    }
+}
