5f01099e5ea94caf93e7b50f6beec8a3e8d5e1d7
gregop <gregopdev@gmail.com>
2023-10-28 14:49:14 +0300

    Generic method to parse paylod data

---
+++ Validators/FitnessApp.Core.Validators/PayloadParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Core.Validators
+{
+    public static class PayloadParser<T>
+    {
+        private static string? _payload;
+
+        private static T? _parsedPayload;
+
+        public static OperationalResult<T?> TryParse(string payload)
+        {
+            try
+            {
+                if (payload == null) { throw new NullReferenceException(); }
+
+                _payload = payload.Trim();
+                _parsedPayload = JsonSerializer.Deserialize<T>(_payload);
+
+                return OperationalResult<T?>.SuccessResult(_parsedPayload);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception at {nameof(PayloadParser<T>)} : {ex.Message}");
+                return OperationalResult<T?>.FailureResult(ex);
+            }
+            
+        }
+
+    }
+}
