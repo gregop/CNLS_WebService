c50150dd4304702a467731dc2ff41b4b78598aee
gregop <gregopdev@gmail.com>
2023-10-28 13:14:30 +0300

    Implementing HandleWorkoutRequestAsync method

--- Engines/FitnessApp.Core.Engines/WorkoutItemEngine.cs
+++ Engines/FitnessApp.Core.Engines/WorkoutItemEngine.cs
@@ -1,6 +1,7 @@
 ﻿using FitnessApp.Core.DataObjects;
 using FitnessApp.Core.Engines.Interfaces;
 using FitnessApp.Core.ResourceAccess;
+using FitnessApp.Core.ResourceAccess.Interfaces;
 using FitnessApp.Core.Validators;
 using System;
 using System.Collections.Generic;
@@ -12,19 +13,31 @@ namespace FitnessApp.Core.Engines
 {
     public class WorkoutItemEngine : IWorkoutItemEngine
     {
-        private readonly WorkoutItemResourceAccess _workoutItemResourceAccess;
+        private readonly IWorkoutItemResourceAccess _workoutItemResourceAccess;
 
-        public WorkoutItemEngine(WorkoutItemResourceAccess workoutItemResourceAccess)
+        public WorkoutItemEngine(IWorkoutItemResourceAccess workoutItemResourceAccess)
         {
             _workoutItemResourceAccess = workoutItemResourceAccess;
 
         }
 
-        public Task<OperationalResult<WorkoutItemDataObject>> HandleWorkoutCreationAsync(WorkoutItemDataObject workoutItemDataObject)
+        public async Task<OperationalResult<WorkoutItemDataObject>> HandleWorkoutCreationAsync(WorkoutItemDataObject workoutItemDataObject)
         {
             throw new NotImplementedException();
         }
 
+        public async Task<OperationalResult<WorkoutItemDataObject>> HandleWorkoutRequestAsync(WorkoutItemDataObject workoutItemDataObject)
+        {
+            try
+            {
+               return await _workoutItemResourceAccess.GetWorkoutItemAsync(workoutItemDataObject);
 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                return OperationalResult<WorkoutItemDataObject>.FailureResult(ex);
+            }
+        }
     }
 }
