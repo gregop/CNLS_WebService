7d376d07a86f94898275b4cb6a71c32e2daeba8e
gregop <gregopdev@gmail.com>
2023-11-01 00:29:44 +0200

    Create Interface and class for Exercice Item Engine

---
+++ Engines/FitnessApp.Core.Engines/ExerciseItemEngine.cs
@@ -0,0 +1,32 @@
+﻿using FitnessApp.Core.DataObjects;
+using FitnessApp.Core.Engines.Interfaces;
+using FitnessApp.Core.ResourceAccess.Interfaces;
+using FitnessApp.Core.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Core.Engines
+{
+    public class ExerciseItemEngine : IExerciseItemEngine
+    {
+        private readonly IExerciseItemResourceAccess _exerciseItemResourceAccess;
+
+        public ExerciseItemEngine(IExerciseItemResourceAccess exerciseItemResourceAccess)
+        {
+            _exerciseItemResourceAccess = exerciseItemResourceAccess;
+        }
+
+        public async Task<OperationalResult<ExerciseItemDataObject>> HandleExerciseCreationAsync(ExerciseItemDataObject exerciseItemDataObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<OperationalResult<ExerciseItemDataObject>> HandleExerciseRequestAsync(ExerciseItemDataObject exerciseItemDataObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Engines/FitnessApp.Core.Engines/Interfaces/IExerciseItemEngine.cs
@@ -0,0 +1,11 @@
+﻿using FitnessApp.Core.DataObjects;
+using FitnessApp.Core.Validators;
+
+namespace FitnessApp.Core.Engines.Interfaces
+{
+    public interface IExerciseItemEngine
+    {
+        Task<OperationalResult<ExerciseItemDataObject>> HandleExerciseCreationAsync(ExerciseItemDataObject exerciseItemDataObject);
+        Task<OperationalResult<ExerciseItemDataObject>> HandleExerciseRequestAsync(ExerciseItemDataObject exerciseItemDataObject);
+    }
+}
