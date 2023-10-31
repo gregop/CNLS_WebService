7a15595a10cc1c53fbfac345ccdcc81925810231
gregop <gregopdev@gmail.com>
2023-11-01 00:30:07 +0200

    Create Interface and class for Exercice Item Orchestrator

---
+++ Orchestrators/FitnessApp.Core.Orchestrators/ExerciseMessagesOrchestrator.cs
@@ -0,0 +1,30 @@
+﻿using FitnessApp.Core.DataObjects;
+using FitnessApp.Core.Orchestrators.Interfaces;
+using FitnessApp.Core.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Core.Orchestrators
+{
+    public class ExerciseMessagesOrchestrator : IExerciseMessagesOrchestrator
+    {
+        public ExerciseMessagesOrchestrator()
+        {
+
+        }
+
+        public async Task<OperationalResult<ExerciseItemDataObject>> HandleExerciseCreationMessagesAsync(string payload)
+        {
+            return null;
+        }
+
+        public async Task<OperationalResult<ExerciseItemDataObject>> HandleExerciseUpdateMessagesAsync(string payload)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ Orchestrators/FitnessApp.Core.Orchestrators/Interfaces/IExerciseMessagesOrchestrator.cs
@@ -0,0 +1,11 @@
+﻿using FitnessApp.Core.DataObjects;
+using FitnessApp.Core.Validators;
+
+namespace FitnessApp.Core.Orchestrators.Interfaces
+{
+    public interface IExerciseMessagesOrchestrator
+    {
+        Task<OperationalResult<ExerciseItemDataObject>> HandleExerciseCreationMessagesAsync(string payload);
+        Task<OperationalResult<ExerciseItemDataObject>> HandleExerciseUpdateMessagesAsync(string payload);
+    }
+}
