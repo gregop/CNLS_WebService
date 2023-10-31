e1cf99eedac7b5553798dfaa2a4f1112c0d1102c
gregop <gregopdev@gmail.com>
2023-11-01 00:28:02 +0200

    Create Interface and class for Exercise creation API response

---
+++ DataObjects/FitnessApp.Core.DataObjects/APIResponses/CreateExerciceItemApiRes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using FitnessApp.Core.DataObjects.Interfaces;
+
+namespace FitnessApp.Core.DataObjects.APIResponses
+{
+    public class CreateExerciceItemApiRes : ICreateExerciceItemApiRes
+    {
+        private string _status;
+
+        private string? _message;
+
+        private ExerciseItemDataObject? _exerciseItem;
+
+        public string Status { get { return _status; } }
+
+        public string? Message { get { return _message; } }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public ExerciseItemDataObject ExerciseItem { get { return _exerciseItem; } set { } }
+
+        public void StatusOK()
+        {
+            _status = "Success";
+            _message = "Success";
+        }
+
+        public void StatusNOK()
+        {
+            _status = "Error";
+        }
+
+        public void SetMessage(string message)
+        {
+            _message = message;
+        }
+
+        public void SetWorkoutItem(ExerciseItemDataObject exerciseItem)
+        {
+            _exerciseItem = exerciseItem;
+        }
+    }
+}
---
+++ DataObjects/FitnessApp.Core.DataObjects/Interfaces/ICreateExerciceItemApiRes.cs
@@ -0,0 +1,15 @@
+﻿namespace FitnessApp.Core.DataObjects.Interfaces
+{
+    public interface ICreateExerciceItemApiRes
+    {
+        string? Message { get; }
+        string Status { get; }
+
+        ExerciseItemDataObject ExerciseItem { get; set; }
+
+        void SetMessage(string message);
+        void SetWorkoutItem(ExerciseItemDataObject exerciseItem);
+        void StatusNOK();
+        void StatusOK();
+    }
+}
