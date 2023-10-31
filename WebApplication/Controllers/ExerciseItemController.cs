57f91826388ae61c2c800ebf890214634b2d2961
gregop <gregopdev@gmail.com>
2023-11-01 00:31:33 +0200

    return type of CreateExercice endpoint

---
+++ WebApplication/Controllers/ExerciseItemController.cs
@@ -0,0 +1,34 @@
+﻿using FitnessApp.Core.DataObjects.Interfaces;
+using FitnessApp.Core.Orchestrators.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebService.Core.Web.Controllers
+{
+    [ApiController]
+    public class ExerciseItemController : ControllerBase
+    {
+        private readonly IExerciseMessagesOrchestrator _exerciseMessagesOrchestrator;
+
+        public ExerciseItemController(IExerciseMessagesOrchestrator exerciseMessagesOrchestrator)
+        {
+            _exerciseMessagesOrchestrator = exerciseMessagesOrchestrator;
+        }
+
+        [HttpPost]
+        [Route("api/[action]")]
+        [Produces("application/json")]
+        public async Task<ActionResult<ICreateExerciceItemApiRes>> CreateExercise(object requestData)
+        {
+
+            return StatusCode(StatusCodes.Status200OK);
+        }
+
+    }
+}
