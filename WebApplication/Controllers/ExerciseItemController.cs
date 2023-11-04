f74be49683ed7dbcc36c42f87940b7ac70934a50
gregop <gregopdev@gmail.com>
2023-11-04 12:07:58 +0200

    CreateExercise endpoint implementation

--- WebApplication/Controllers/ExerciseItemController.cs
+++ WebApplication/Controllers/ExerciseItemController.cs
@@ -1,12 +1,16 @@
 ﻿using FitnessApp.Core.DataObjects.Interfaces;
 using FitnessApp.Core.Orchestrators.Interfaces;
+using FitnessApp.Core.Validators;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace WebService.Core.Web.Controllers
@@ -27,7 +31,40 @@ namespace WebService.Core.Web.Controllers
         public async Task<ActionResult<ICreateExerciceItemApiRes>> CreateExercise(object requestData)
         {
 
-            return StatusCode(StatusCodes.Status200OK);
+            try 
+            {
+                if(object.ReferenceEquals(requestData, null))
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest);
+
+                }
+                
+                string requestDataSerialized = JsonSerializer.Serialize(requestData);
+
+                OperationalResult<ResponseContext<ICreateExerciceItemApiRes>> response = await _exerciseMessagesOrchestrator.HandleExerciseCreationMessagesAsync(requestDataSerialized);
+
+
+                if (response.IsSuccessfulOperation && response.Data?.Response != null)
+                {
+                    return StatusCode(StatusCodes.Status200OK, response.Data.Response);
+                }
+                else if (response.IsSuccessfulOperation && response.Data?.Response == null)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, response.Data);
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                }
+
+               
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
         }
 
     }
