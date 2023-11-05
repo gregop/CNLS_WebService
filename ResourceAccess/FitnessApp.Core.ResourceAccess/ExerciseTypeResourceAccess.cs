e22071c9ddf35d2624656af349b723fa13ff1fdd
gregop <gregopdev@gmail.com>
2023-11-05 14:06:57 +0200

    Interface and class for ExerciseTypeResourceAccess

---
+++ ResourceAccess/FitnessApp.Core.ResourceAccess/ExerciseTypeResourceAccess.cs
@@ -0,0 +1,38 @@
+﻿using FitnessApp.Core.DataObjects;
+using FitnessApp.Core.ResourceAccess.DbContexts;
+using FitnessApp.Core.ResourceAccess.Interfaces;
+using FitnessApp.Core.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Core.ResourceAccess
+{
+    internal class ExerciseTypeResourceAccess : IExerciseTypeResourceAccess
+    {
+
+        private readonly ExerciseItemDbContext _dbContext;
+
+        public ExerciseTypeResourceAccess(ExerciseItemDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<OperationalResult<List<ExerciseTypeDataObject>>> GetAllExerciseItemsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<OperationalResult<ExerciseTypeDataObject>> LogExerciseItemAsync(ExerciseTypeDataObject dataObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<OperationalResult<ExerciseTypeDataObject>> UpdateExerciseItemAsync(ExerciseTypeDataObject dataObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ResourceAccess/FitnessApp.Core.ResourceAccess/Interfaces/IExerciseTypeResourceAccess.cs
@@ -0,0 +1,18 @@
+﻿using FitnessApp.Core.DataObjects;
+using FitnessApp.Core.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Core.ResourceAccess.Interfaces
+{
+    public interface IExerciseTypeResourceAccess
+    {
+        Task<OperationalResult<List<ExerciseTypeDataObject>>> GetAllExerciseItemsAsync();
+        Task<OperationalResult<ExerciseTypeDataObject>> LogExerciseItemAsync(ExerciseTypeDataObject dataObject);
+
+        Task<OperationalResult<ExerciseTypeDataObject>> UpdateExerciseItemAsync(ExerciseTypeDataObject dataObject);
+    }
+}
